89f0d1abd74ab5aba7ac451c3c7281b47dfb2d07
Liam Neville <lineville@github.com>
2024-08-20 13:59:39 -0700

    update comments on ai module

--- src/index.ts
+++ src/index.ts
@@ -151,10 +151,9 @@ export const main = async (port: number) => {
 
         await new Promise((resolve) => setTimeout(resolve, 1000));
 
-        // TODO handle AI guessing character
-        // if (state.eliminatedCharacters.get("AI")!.size >= NUM_CHARACTERS - 5) {
+        // TODO handle AI guessing character -- need some proxy for how confident we are it's each of the remaining characters
+        // if one of the characters probability exceeds a certain threshold, guess it
 
-        // } else {
         const aiQuestion = generateQuestion(
           state.characters,
           state.eliminatedCharacters.get("AI")!
@@ -166,7 +165,6 @@ export const main = async (port: number) => {
         console.log(
           `❓ AI asked: ${aiQuestion} [ClientID: ${clientId}] [GameID: ${gameId}]`
         );
-        // }
       }
     });
 
